176db64b1eff9594d2a4dde9d569c5988f2a6fa2
Israajddn <Israajddn@gmail.com>
2023-12-15 10:52:43 -0500

    pizza App with edits

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Pizza from './pizza';
+import Pizza from './Pizza';
 
 function Menu(props) {
   console.log(props.pizzaData);
   return (
     <main className='menu'>
         <h2>Our Menu</h2>
-        {props.pizzaData.map((pizza)=>(
-          <Pizza {...pizza}/>
+        {props.pizzaData.map((pizza, i)=>(
+          <Pizza key={i} {...pizza}/>
         )
         )}
     </main>
